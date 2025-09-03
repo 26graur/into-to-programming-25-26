b9456f3038f263b40aaeb934d88bd2f21c4ad682
Grau <26GrauR@badinhs.org>
2025-09-03 14:21:56 -0400

    practiced console.log

--- variablesnotes.js
+++ variablesnotes.js
@@ -1,1 +1,40 @@
 // this is a comment. it is visible to the user but not to the computer. they're very useful.
+
+// console.log("Hello, world")
+
+// let fish;
+// fish = 3;
+// let cats = 7;
+
+// console.log(fish)
+// console.log(cats)
+// console.log(cats + fish)
+
+// let first = 1;
+// const second = 2;
+// var third = 3;
+
+// console.log(first)
+// console.log(second)
+// console.log(third)
+
+// first = 1 + 1
+// console.log(first)
+
+// first = 7;
+// second = 8;
+// third = 9;
+
+// console.log(first)
+// console.log(second)
+// console.log(third)
+
+// OPERATIONS are +, -, *, /, ** (exponents), % (modular)
+// console.log(3 + 5)
+// console.log(8 / 3)
+// console.log(35 + 5 / 4)
+
+let max = 57;
+let actual = max - 13
+let percentage = actual / max
+console.log(percentage)
