bf02f6c54c076a50350cbc92113a55b79c959519
Grau <26GrauR@badinhs.org>
2025-09-12 14:30:04 -0400

    took notes on 9/12/25

--- datatypenotes.js
+++ datatypenotes.js
@@ -5,7 +5,7 @@
 // in a dynamiaclly typed 
 
 // // example
-// let message = "hello";
+let message = "hello";
 // message = 123;`
 
 // Number:
@@ -19,14 +19,14 @@
 
 // BigInt, Big Intigers, represents big intigers
 // "number" as a datatype can't sore a value larger that (2^53 - 1) = 9,007,199,254,740,991
-const sampleBigInt = 123456789123456789123456789n;
+// const sampleBigInt = 123456789123456789123456789n;
 // final "n" indicates that it's a BigInt
 
 // String:
 // a string is text.
 // there are three ways to produce a string in JavaScript which are slightly differant.
-let string1 = 'this is a string'
-let string2 = "this is also a string"
+// let string1 = 'this is a string'
+// let string2 = "this is also a string"
 // you usually select what string notation you're using based on the content of your string
 // for example:
 // let dialogue = "the man yelled, "here we go"" (won't work)
@@ -36,7 +36,7 @@ let string2 = "this is also a string"
 let name = "Raynah";
 alert(`Hello, ${name}!`);
 // backticks (`) allow you to embed code into the string
-console.log(`3 + 4 is equal to ${3 + 4}`);
+// console.log(`3 + 4 is equal to ${3 + 4}`);
 
 // char doesn't exist in JavaScript but does exist in C and Java. 
 // It's basically a string of length 1.
@@ -44,16 +44,16 @@ console.log(`3 + 4 is equal to ${3 + 4}`);
 // Boolean Values:
 // named for George Boolean
 // has two values: True and False
-let nameFieldValid = true;      // the person imput a valid name
-let ageFieldValue = false;      // the person did not imput a valid age
-console.log(3 < 1);     // will return false since it's inacurate
+// let nameFieldValid = true;      // the person imput a valid name
+// let ageFieldValue = false;      // the person did not imput a valid age
+// console.log(3 < 1);     // will return false since it's inacurate
 
 //null:
 // contains only one value: "null"
 // means nothing, empty, value unknown
-let age = null;
+// let age = null;
 // not to be confused with 'undefined", which means that a value has not been assigned
-let fish;       // will come back as undefined
+// let fish;       // will come back as undefined
 
 // objects and symbols:
 // objects can hold complex date structures
@@ -65,22 +65,46 @@ let fish;       // will come back as undefined
 
 // string methods:
 // Length: technecally a property, not strictly a method.
-let text = "bvcfrtyujnbv";
-console.log(text.length)
-//ways to extract characters from a string:
-// at():                            // prefered and most useful
+// let text = "bvcfrtyujnbv";
+// console.log(text.length)
+// ways to extract characters from a string:
+// .at():                            // prefered and most useful
 text = "Stevie T. Badin";
-console.log(text.charAt(3));
-console.log(text.at(-3));
+// console.log(text.charAt(3));
+// console.log(text.at(-3));
 // charCodeAt():                    // returns the UTF-16 code of the character at the specified index
-console.log(text.charCodeAt(3));
+// console.log(text.charCodeAt(3));
 //.concat:                          // stands for concatination. allows you to concatinate two or more strings
-let text1 = "hello";
-let text2 = "Stevie";
-text3 = text1.concat(" ", text2);
-console.log(text3);
-text4 = text1 + " " + text2;
-console.log(text4);
+// let text1 = "hello";
+// let text2 = "Stevie";
+// text3 = text1.concat(" ", text2);
+// console.log(text3);
+// text4 = text1 + " " + text2;
+// console.log(text4);
+
 // if you want to extract a sub-string you can use 3 methods.
 // slice():                     // returns a strin g that starts from the given index and ends before the second index
-console.log(text.slice(3, 8));
+// console.log(text.slice(3, 8));
+// if you leave out the second value you will get the rest of the string
+// you can also usd negetive integers
+// console.log(text.slice(-5))
+
+// [] this is a notation that allows you to use something similar to .at
+
+// .substring();
+// similar to slice but start and end values that are less than 0 are treated as zero
+// .substr();           // where the second number was how long you want the string to be
+// this is depricated, so don't use it
+
+// .toUpperCase()
+console.log(text.toUpperCase())
+// .toLowerCase()
+console.log(text.toLowerCase())
+
+text = "                            Stevie T. Badin"
+console.log(text);
+
+// .trim()
+// used to remove white space from both ends of a string
+console.log(text.trim());
+// can also do .trimStart and .trimEnd
